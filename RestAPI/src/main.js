60527cb9e0363bdd778c78afda5f5a69486716de
l0u0h0 <l0u0h0@gmail.com>
2022-08-24 22:38:43 +0900

    regexp_add

--- RestAPI/src/main.js
+++ RestAPI/src/main.js
@@ -6,8 +6,34 @@
  */
 
 const http = require("http");
+/**
+ * Post
+ *
+ * GET /posts
+ * GET /posts/:id
+ * POST /posts
+ */
 
 const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  // console.log("Requst accepted");
+  if (req.url === "/posts" && req.method === "GET") {
+    res.statusCode = 200;
+    res.end("List of Posts");
+    return;
+  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+    res.statusCode = 200;
+    res.end("Some content of the post");
+    return;
+  } else if (req.url === "/posts" && req.method === "POST") {
+    res.statusCode = 200;
+    res.end("Creating post");
+    return;
+  } else {
+    res.statusCode = 404;
+    res.end("Not Found");
+    return;
+  }
   res.statusCode = 200;
   res.end("hello");
 });
