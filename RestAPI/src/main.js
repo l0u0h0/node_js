7a7cc58ae53019c1dd8cb7db5a18407d0cb9864b
l0u0h0 <l0u0h0@gmail.com>
2022-08-25 16:22:08 +0900

    Posting

--- RestAPI/src/main.js
+++ RestAPI/src/main.js
@@ -23,7 +23,7 @@ const posts = [
   },
   {
     id: "my_second_post",
-    title: "My Second Post",
+    title: "나의 두번째 포스트",
     content: "Second post!",
   },
 ];
@@ -42,18 +42,47 @@ const server = http.createServer((req, res) => {
     (req.url && POST_ID_REGEX.exec(req.url)) || undefined;
   // console.log(req.url);
   // console.log("Requst accepted");
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
   if (req.url === "/posts" && req.method === "GET") {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalCount: posts.length,
+    };
     res.statusCode = 200;
-    res.end("List of Posts");
+    res.end(JSON.stringify(result));
     return;
-  } else if (postIdRegexResult) {
+  } else if (postIdRegexResult && req.method === "GET") {
     // GET /posts/:id
     const postId = postIdRegexResult[1];
-    console.log(`postId: ${postId}`);
-    res.statusCode = 200;
-    res.end("Some content of the post");
+    const post = posts.find((_post) => _post.id === postId);
+
+    if (post) {
+      res.statusCode = 200;
+      res.end(JSON.stringify(post));
+    } else {
+      res.statusCode = 404;
+      res.end("POST Not Found");
+    }
     return;
   } else if (req.url === "/posts" && req.method === "POST") {
+    req.setEncoding("utf-8");
+    req.on("data", (data) => {
+      /**
+       * @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       */
+      /** @type {CreatePostBody} */
+      const body = JSON.parse(data);
+      posts.push({
+        id: body.title.toLowerCase().replace(/\s/g, "_"),
+        title: body.title,
+        content: body.content,
+      });
+    });
     res.statusCode = 200;
     res.end("Creating post");
     return;
