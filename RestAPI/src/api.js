76c2789d1d535324a9e9f30da52ad287f79c1347
l0u0h0 <l0u0h0@gmail.com>
2022-08-26 19:32:50 +0900

    refactoring

--- RestAPI/src/api.js
+++ RestAPI/src/api.js
@@ -31,7 +31,7 @@ const posts = [
  * @typedef Route
  * @property {RegExp} url
  * @property {'GET' | 'POST'} method
- * @property {(values: object) => Promise<APIResponse>} callback
+ * @property {(matches: string[], body: Object.<string, *> | undefined) => Promise<APIResponse>} callback
  */
 
 /** @type {Route[]} */
@@ -40,28 +40,61 @@ const routes = [
     url: /^\/posts$/,
     method: "GET",
     callback: async () => ({
-      // TODO: implement
       statusCode: 200,
-      body: "Heeoo",
+      body: posts,
     }),
   },
   {
     url: /^\/posts\/([a-zA-Z0-9-_]+)$/,
     method: "GET",
-    callback: async () => ({
-      // TODO: implement
-      statusCode: 200,
-      body: {},
-    }),
+    callback: async (matches) => {
+      const postId = matches[1];
+      if (!postId) {
+        return {
+          statusCode: 404,
+          body: "Not Found.",
+        };
+      }
+
+      const post = posts.find((_post) => _post.id === postId);
+
+      if (!post) {
+        return {
+          statusCode: 404,
+          body: "Not Found.",
+        };
+      }
+      return {
+        statusCode: 200,
+        body: post,
+      };
+    },
   },
   {
     url: /^\/posts$/,
     method: "POST",
-    callback: async () => ({
-      // TODO: implement
-      statusCode: 200,
-      body: {},
-    }),
+    callback: async (_, body) => {
+      if (!body) {
+        return {
+          statusCode: 400,
+          body: "Ill-formed Request.",
+        };
+      }
+
+      /** @type {string} */
+      const title = body.title;
+      const newPost = {
+        id: title.replace(/\s/g, "_"),
+        title,
+        content: body.content,
+      };
+
+      posts.push(newPost);
+      return {
+        statusCode: 200,
+        body: newPost,
+      };
+    },
   },
 ];
 
--- RestAPI/src/main.js
+++ RestAPI/src/main.js
@@ -6,6 +6,7 @@
  */
 
 const http = require("http");
+const { resolve } = require("path");
 
 const { routes } = require("./api");
 
@@ -19,13 +20,36 @@ const server = http.createServer((req, res) => {
         _route.method === req.method
     );
 
-    if (!route) {
+    if (!req.url || !route) {
       res.statusCode = 404;
       res.end("Not Found");
       return;
     }
 
-    const result = await route.callback();
+    const regexResult = route.url.exec(req.url);
+
+    if (!regexResult) {
+      res.statusCode = 404;
+      res.end("Not Found");
+      return;
+    }
+
+    /** @type {Object.<string, *> | undefined} */
+    const reqBody =
+      (req.headers["content-type"] === "application/json" &&
+        (await new Promise((resolve, reject) => {
+          req.setEncoding("utf-8");
+          req.on("data", (data) => {
+            try {
+              resolve(JSON.parse(data));
+            } catch {
+              reject(new Error("Ill-formed json"));
+            }
+          });
+        }))) ||
+      undefined;
+
+    const result = await route.callback(regexResult, reqBody);
     res.statusCode = result.statusCode;
     if (typeof result.body === "string") {
       res.end(result.body);
